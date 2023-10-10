2664eec34233beb21bb6b701e2496f015ef82592
Dmitry O <theicesilver121@mail.ru>
2023-10-10 09:50:36 +0300

    8

--- components/masthead/components/slider/index.tsx
+++ components/masthead/components/slider/index.tsx
@@ -228,12 +228,7 @@ const Slider: React.FC = () => {
 
                         if (totalImages == loadedImages) {
                             gsap.timeline()
-                                .to('.loading__wrapper', {
-                                    duration: 0.8,
-                                    opacity: 0,
-                                    pointerEvents: 'none',
-                                })
-                                .call(() => init());
+                            .call(() => init());
                         }
                     }
                 });
